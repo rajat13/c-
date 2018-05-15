3b13bec953f78e6d1a87b3bb1ae7de7a1e7af5ae
Rajat GOYAL <Rajat.GOYAL@amadeus.com>
2018-05-15 12:20:12 +0530

    cin weird behaviour

--- second.cpp
+++ second.cpp
@@ -2,9 +2,10 @@
 #include<string>
 using namespace std;
 int main(){
+	{
 	string name;
 	cout<<"Enter same name again"<<endl;
-	getline(cin,name);
+	getline(cin,name);}
 	const string third="Hello "+name;
 	const string first(third.size()+4,'*');
 	const string spaces(third.size(),' ');
@@ -13,6 +14,6 @@ int main(){
 	cout<<"* "+third+" *"<<endl;
 	cout<<"* "<<spaces<<" *"<<endl;
 	cout<<first<<endl;
-	
+	cout<<"Rajat"<<endl;
 	return 0;
 }
